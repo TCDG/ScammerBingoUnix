9e8da864f4aad0c18e0d0b21f262f890ac136c96
afroraydude <rayjo5000@yahoo.com>
2016-08-25 17:09:14 -0400

    Alpha 1

---
+++ Assets/Scripts/ButtonDisabler.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonDisabler : MonoBehaviour
+{
+    public Button[] buttons = new Button[16];
+    int score = 0;
+    // Use this for initialization
+    public Text scoreUI;
+    
+    void Start()
+    {
+        scoreUI = scoreUI.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreUI.text = "Score: " + score.ToString();
+    }
+
+    public void BtnOnClick(int id)
+    {
+        buttons[id].enabled = false;
+        buttons[id].interactable = false;
+        buttons[id].image.color = Color.gray;
+        print("Button " + id + " clicked!");
+        score++;
+    }
+
+    public void Reset()
+    {
+        /**
+        bool resetDone = false;
+        int i = 0;
+        while (!resetDone)
+        {
+            buttons[i].enabled = true;
+            buttons[i].interactable = true;
+            i++;
+            if(i > 16)
+            {
+                resetDone = true;
+            }
+
+        }*/
+        Application.LoadLevel(0);
+    }
+}
